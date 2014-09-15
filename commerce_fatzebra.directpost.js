d8133556fc84cd47be3d52e1e4a691c13e060e65
Mario Rogic <hello@mario.net.au>
2014-09-15 22:41:01 +1000

    Bug fixes for direct post, now working all the way through with test config

--- commerce_fatzebra.directpost.js
+++ commerce_fatzebra.directpost.js
@@ -1,8 +1,18 @@
 (function ($) {
   Drupal.behaviors.commerce_fatzebra = {
     _processed: false,
+    _processing: false,
 
     tokenize: function(submit) {
+      // Terminate early if we're already processing a request
+      if (this._processing) { return false; }
+
+      // Skip to submit if we're finished
+      if (this._processed) { return true; }
+
+      // Start processing a new request
+      this._processing = true;
+
       // Remember the form so we can trigger the submit later
       this._submit = submit;
 
@@ -14,17 +24,18 @@
         expiry_year: '2019',
         cvv: '878',
         return_path: 'https://gateway.sandbox.fatzebra.com.au',
-        verification: '0dd46be9107ac9c4483302f156a642cd'
+        verification: '0ccd3290f9ed250f327930b568453b29'
       };
 
       $.ajax({
         type: "GET",
-        url: "https://gateway.sandbox.fatzebra.com.au/v2/credit_cards/direct/TEST.json",
+        url: "https://gateway.sandbox.fatzebra.com.au/v2/credit_cards/direct/TESTmario.json",
         data: req,
         jsonpCallback: "Drupal.behaviors.commerce_fatzebra.jsonp",
         contentType: "text/javascript",
         dataType: "jsonp",
         error: function(e) {
+          // @TODO need to handle processing loop here too
           // Handle errors here - all non HTTP 2xx errors, such as HTTP 500, 403, 401 etc
           // (very unlikely, but HTTP 500 or HTTP 502 is the most likely to happen)
           alert(e.message);
@@ -46,6 +57,7 @@
         $('#payment_details_token').val(data.token);
       }
 
+      this._processing = false;
       this._processed = true;
       $(this._submit).submit();
     }
